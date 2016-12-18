8af5b1ee052fedbdbda9544dd913d8fcc9dd4a38
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2016-12-18 13:51:17 -0200

    Adição da classe ViaCepResposta que representa a resposta de uma requisição.

---
+++ MosaicoSolutions.ViaCep/Net/ViaCepResposta.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Http;
+
+namespace MosaicoSolutions.ViaCep.Net
+{
+    /// <summary>
+    /// Representa a resposta de uma requisiçao.
+    /// </summary>
+    public sealed class ViaCepResposta
+    {
+        private readonly HttpResponseMessage _responseMessage;
+
+        internal ViaCepResposta(HttpResponseMessage responseMessage)
+        {
+            _responseMessage = responseMessage;
+        }
+
+        public HttpStatusCode CodigoDeStatus => _responseMessage.StatusCode;
+
+        public bool EhCodigoDeSucesso => CodigoDeStatus == HttpStatusCode.OK;
+
+        public ViaCepConteudo ObterConteudo() => new ViaCepConteudo(_responseMessage.Content);
+    }
+}
