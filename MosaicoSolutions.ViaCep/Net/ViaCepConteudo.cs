c84b7ecd008ef504c74f7b17dcb281e7729bba0a
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2016-12-18 13:50:41 -0200

    Adição da classe ViaCepConteudo que representa o conteúdo da resposta de uma requisição.

---
+++ MosaicoSolutions.ViaCep/Net/ViaCepConteudo.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Net.Http;
+using System.Xml.Linq;
+
+namespace MosaicoSolutions.ViaCep.Net
+{
+    /// <summary>
+    /// Representa o conteúdo de uma requisisão.
+    /// </summary>
+    public class ViaCepConteudo
+    {
+
+        private readonly string _conteudo;
+
+        internal ViaCepConteudo(HttpContent httpContent)
+        {
+            _conteudo = httpContent.ReadAsStringAsync().Result;
+        }
+
+        /// <summary>
+        /// Lê o conteúdo como xml.
+        /// </summary>
+        /// <returns>Um Objeto do tipo <code>XDocument</code>.</returns>
+        /// <exception cref="InvalidOperationException">Se não for possivel ler o conteúdo como xml.</exception>
+        public XDocument LerComoXml()
+        {
+            if (!_conteudo.Contains("xmlcep"))
+                throw new InvalidOperationException("Não é possivel ler o conteúdo como xml.");
+
+            return XDocument.Parse(_conteudo);
+        }
+
+        /// <summary>
+        /// Lê o conteúdo como <code>string</code>.
+        /// </summary>
+        /// <returns>Uma string que representa o conteúdo da requisição.</returns>
+        public string LerComoString() => _conteudo;
+
+        /// <summary>
+        /// Testa se o conteúdo possui erro.
+        /// </summary>
+        /// <returns>true, se possuir erro, caso contrário, false.</returns>
+        public bool PossuiErro() => _conteudo.Contains("erro");
+
+    }
+}
