d652e5a366f76cf78003e77b32dc60d9ec46d975
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2016-12-18 13:46:35 -0200

    Adição da constante QuantidadeMinimaDeCaracteres.

--- MosaicoSolutions.ViaCep/Net/EnderecoRequisicao.cs
+++ MosaicoSolutions.ViaCep/Net/EnderecoRequisicao.cs
@@ -14,12 +14,17 @@ namespace MosaicoSolutions.ViaCep.Net
 
         public bool EhValido() => EhCidadeValida() && EhLogradouroValido();
 
-        private bool EhCidadeValida() => !string.IsNullOrEmpty(Cidade) && Cidade.Length >= 3;
+        private bool EhCidadeValida() => !string.IsNullOrEmpty(Cidade) && Cidade.Length >= QuantidadeMinimaDeCaracteres;
 
-        private bool EhLogradouroValido() => !string.IsNullOrEmpty(Logradouro) && Logradouro.Length >= 3;
+        private bool EhLogradouroValido() => !string.IsNullOrEmpty(Logradouro) && Logradouro.Length >= QuantidadeMinimaDeCaracteres;
 
         public UF UF { get; set; }
         public string Cidade { get; set; }
         public string Logradouro { get; set; }
+
+        /// <summary>
+        /// Quantidade de Caracteres mínimo da Cidade e Logradouro para uma requisicao por endereço válida.
+        /// </summary>
+        public const int QuantidadeMinimaDeCaracteres = 3;
     }
 }
