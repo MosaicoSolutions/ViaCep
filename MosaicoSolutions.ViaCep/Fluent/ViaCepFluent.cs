57b94c4bfd6c1ed5effced9239c1da58ffc884f2
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-01-07 17:53:32 -0200

    Classe que fornece uma forma de realizar requisições baseado em Fluent Interface.

---
+++ MosaicoSolutions.ViaCep/Fluent/ViaCepFluent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MosaicoSolutions.ViaCep.Modelos;
+using MosaicoSolutions.ViaCep.Net;
+
+namespace MosaicoSolutions.ViaCep.Fluent
+{
+    public static class ViaCepFluent
+    {
+        public static ViaCepFluentPorCep ObterPorCep(string cep)
+            => new ViaCepFluentPorCep(new Cep(cep));
+
+        public static ViaCepFluentPorCep ObterPorCep(Cep cep)
+        {
+            if (cep == null)
+                throw new ArgumentNullException(nameof(cep));
+
+            return new ViaCepFluentPorCep(cep);
+        }
+
+        public static ViaCepFluentPorEndereco ObterPorEndereco(EnderecoRequisicao enderecoRequisicao)
+            => new ViaCepFluentPorEndereco(enderecoRequisicao);
+    }
+}
