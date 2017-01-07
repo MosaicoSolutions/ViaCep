fecf2fb7ce0a97f3edaeaa2fc5a6cee1082260cb
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-01-07 17:48:04 -0200

    Classe que fornece métodos de conversão para o tipo Endereco.

---
+++ MosaicoSolutions.ViaCep/Util/EnderecoConvert.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using MosaicoSolutions.ViaCep.Modelos;
+
+namespace MosaicoSolutions.ViaCep.Util
+{
+    public static class EnderecoConvert
+    {
+        public static Endereco DeJsonParaEndereco(string json)
+            => Newtonsoft.Json.JsonConvert.DeserializeObject<Endereco>(json);
+
+        public static IEnumerable<Endereco> DeJsonParaListaDeEnderecos(string json)
+            => Newtonsoft.Json.JsonConvert.DeserializeObject<List<Endereco>>(json);
+    }
+}
