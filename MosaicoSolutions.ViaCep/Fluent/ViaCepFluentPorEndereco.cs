5f9be9397ef321f25b527445c8d73e76d6d16cf1
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-01-07 17:56:09 -0200

    Classe que implementa a Fluent Interface por Endereco.

---
+++ MosaicoSolutions.ViaCep/Fluent/ViaCepFluentPorEndereco.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using MosaicoSolutions.ViaCep.Fluent.Interfaces;
+using MosaicoSolutions.ViaCep.Modelos;
+using MosaicoSolutions.ViaCep.Net;
+
+namespace MosaicoSolutions.ViaCep.Fluent
+{
+    //TODO: Implementar métodos restantes.
+    public class ViaCepFluentPorEndereco : IViaCepFluentPorEndereco
+    {
+        private readonly EnderecoRequisicao _enderecoRequisicao;
+
+        internal ViaCepFluentPorEndereco(EnderecoRequisicao enderecoRequisicao)
+        {
+            _enderecoRequisicao = enderecoRequisicao;
+        }
+
+        public string ComoJson()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public XDocument ComoXml()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Endereco> ComoListaDeEnderecos()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
