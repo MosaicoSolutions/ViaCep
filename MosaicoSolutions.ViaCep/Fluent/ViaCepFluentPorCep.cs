cfdb334a09fa27dbcbc1bc9066c7f6824f275a91
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-01-07 17:55:27 -0200

    Classe que implementa a Fluent Interface por Cep.

---
+++ MosaicoSolutions.ViaCep/Fluent/ViaCepFluentPorCep.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml.Linq;
+using MosaicoSolutions.ViaCep.Fluent.Interfaces;
+using MosaicoSolutions.ViaCep.Modelos;
+using MosaicoSolutions.ViaCep.Util;
+
+namespace MosaicoSolutions.ViaCep.Fluent
+{
+    //TODO: Implementar métodos restantes.
+    public class ViaCepFluentPorCep : IViaCepFluentPorCep
+    {
+        private readonly Cep _cep;
+
+        internal ViaCepFluentPorCep(Cep cep)
+        {
+            _cep = cep;
+        }
+
+        public Endereco ComoEndereco()
+            => EnderecoConvert.DeJsonParaEndereco(ComoJson());
+
+        public string ComoJson()
+            => ViaCep.ObterEnderecoComoJson(_cep);
+
+        public XDocument ComoXml()
+            => ViaCep.ObterEnderecoComoXml(_cep);
+
+        public string ComoPiped()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ComoQuerty()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
