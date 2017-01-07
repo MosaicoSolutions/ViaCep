f67c2fc4fe4594368c91482c7176bc00b7d1ffe4
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-01-07 17:50:35 -0200

    Interface IViaCepFluentPorCep.

---
+++ MosaicoSolutions.ViaCep/Fluent/Interfaces/IViaCepFluentPorCep.cs
@@ -0,0 +1,7 @@
+﻿namespace MosaicoSolutions.ViaCep.Fluent.Interfaces
+{
+    public interface IViaCepFluentPorCep : IViaCepFluentComoJson, IViaCepFluentComoXml, IViaCepFluentComoPiped, IViaCepFluentComoQuerty, IViaCepFluentComoEndereco
+    {
+
+    }
+}
