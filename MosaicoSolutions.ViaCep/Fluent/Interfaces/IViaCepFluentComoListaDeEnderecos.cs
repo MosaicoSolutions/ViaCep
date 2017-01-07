f147169b82713886fcd672545765745eabe6c9f3
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-01-07 17:49:42 -0200

    Interface IViaCepFluentComoListaDeEnderecos.

---
+++ MosaicoSolutions.ViaCep/Fluent/Interfaces/IViaCepFluentComoListaDeEnderecos.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MosaicoSolutions.ViaCep.Modelos;
+
+namespace MosaicoSolutions.ViaCep.Fluent.Interfaces
+{
+    public interface IViaCepFluentComoListaDeEnderecos
+    {
+        IEnumerable<Endereco> ComoListaDeEnderecos();
+    }
+}
