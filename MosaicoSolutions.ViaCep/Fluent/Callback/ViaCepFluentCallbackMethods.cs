3bcbd281642d02e849b523d22fbbc629680b441a
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-01-12 16:50:00 -0200

    Métodos de extensão para as interfaces IViaCepFluentPorCep e IViaCepFluentPorEndereco.

---
+++ MosaicoSolutions.ViaCep/Fluent/Callback/ViaCepFluentCallbackMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using MosaicoSolutions.ViaCep.Fluent.Interfaces;
+using MosaicoSolutions.ViaCep.Modelos;
+
+namespace MosaicoSolutions.ViaCep.Fluent.Callback
+{
+    public static class ViaCepFluentCallbackMethods
+    {
+        public static void ComoEndereco(this IViaCepFluentPorCep fluent, Action<Endereco> callback)
+            => callback(fluent.ComoEndereco());
+
+        public static void ComoJson(this IViaCepFluentPorCep fluent, Action<string> callback)
+            => callback(fluent.ComoJson());
+
+        public static void ComoPiped(this IViaCepFluentPorCep fluent, Action<string> callback)
+            => callback(fluent.ComoPiped());
+
+        public static void ComoQuerty(this IViaCepFluentPorCep fluent, Action<string> callback)
+            => callback(fluent.ComoQuerty());
+
+        public static void ComoXml(this IViaCepFluentPorCep fluent, Action<XDocument> callback)
+            => callback(fluent.ComoXml());
+
+        public static void ComoJson(this IViaCepFluentPorEndereco fluent, Action<string> callback)
+            => callback(fluent.ComoJson());
+
+        public static void ComoXml(this IViaCepFluentPorEndereco fluent, Action<XDocument> callback)
+            => callback(fluent.ComoXml());
+
+        public static void ComoListaDeEnderecos(this IViaCepFluentPorEndereco fluent, Action<IEnumerable<Endereco>> callback)
+            => callback(fluent.ComoListaDeEnderecos());
+    }
+}
