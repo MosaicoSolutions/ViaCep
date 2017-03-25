6918754f962b1ca72127b17bc7998884a669845b
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-25 01:03:30 -0300

    Ajuste no nome da propriedade Cep.

--- MosaicoSolutions.ViaCep/Modelos/Endereco.cs
+++ MosaicoSolutions.ViaCep/Modelos/Endereco.cs
@@ -5,7 +5,7 @@ namespace MosaicoSolutions.ViaCep.Modelos
     [Serializable]
     public struct Endereco
     {
-        public string CEP { get; set; }
+        public string Cep { get; set; }
         public string Logradouro { get; set; }
         public string Complemento { get; set; }
         public string Bairro { get; set; }
