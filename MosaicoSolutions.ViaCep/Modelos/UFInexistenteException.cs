1977860ff9dcad30b8beaf966396540256c4bf2e
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-25 16:25:35 -0300

    Adição da classe UFInexistenteException

---
+++ MosaicoSolutions.ViaCep/Modelos/UFInexistenteException.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MosaicoSolutions.ViaCep.Modelos
+{
+    /// <summary>
+    /// Exception lançada quando o método <see cref="MosaicoSolutions.ViaCep.Modelos.UF.EncontraPeloCodigo(System.Int32)"/>
+    /// ou <see cref="MosaicoSolutions.ViaCep.Modelos.UF.EncontraPelaSigla(System.String)"/> não encontra a UF com os
+    /// parâmetros especificados.
+    /// </summary>
+    [Serializable]
+    public class UFInexistenteException : Exception
+    {
+        /// <summary>
+        /// Inicializa uma nova instância da classe <see cref="UFInexistenteException"/>.
+        /// </summary>
+        public UFInexistenteException() : base("A UF não existe.")
+        {
+        }
+
+        /// <summary>
+        /// Inicializa uma nova instância da classe <see cref="UFInexistenteException"/> com a mensagem de erro especificada.
+        /// </summary>
+        /// <param name="message">A mensagem de erro que explica o motivo da exceção.</param>
+        public UFInexistenteException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Inicializa uma nova instância da classe <see cref="UFInexistenteException"/> com a mensagem de erro especificada.
+        /// e uma referência para a exceção interna que é a causa dessa exceção.
+        /// </summary>
+        /// <param name="message">A mensagem de erro que explica o motivo para a exceção.</param>
+        /// <param name="innerException">A exceção que é a causa da exceção atual.
+        /// Se o <code>innerException</code> parâmetro não é uma referência nula,
+        /// a exceção atual é gerada em um bloco catch que manipula a exceção interna.
+        /// </param>
+        public UFInexistenteException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Inicializa uma nova instância da classe <see cref="UFInexistenteException"/> com dados serializados.
+        /// </summary>
+        /// <param name="info">O objeto que contém os dados do objeto serializado.</param>
+        /// <param name="context">As informações contextuais sobre a origem ou o destino.</param>
+        protected UFInexistenteException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
