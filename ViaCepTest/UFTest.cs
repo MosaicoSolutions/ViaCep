0e525046740fe1f845c7a8a93d501c5eec0158c9
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-25 00:46:08 -0300

    Teste Unitário da classe UF.

---
+++ ViaCepTest/UFTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using MosaicoSolutions.ViaCep.Modelos;
+using NUnit.Framework;
+
+namespace ViaCepTest
+{
+    [TestFixture]
+    public class UFTest
+    {
+        [Test]
+        public void UFsDevemSerIguais()
+        {
+            var uf = UF.PE;
+            var uf2 = UF.PE;
+
+            Assert.AreEqual(uf, uf2);
+            Assert.True(uf.Equals(uf2));
+        }
+
+        [Test]
+        public void NaoDeveEncontrarUFPeloCodigo()
+            => Assert.Throws<UFInexistenteException>(() => UF.EncontraPeloCodigo(100));
+
+        [Test]
+        public void NaoDeveEncontrarUFPelaSigla()
+            => Assert.Throws<UFInexistenteException>(() => UF.EncontraPelaSigla("LOL"));
+
+        [Test]
+        public void DeveEncontrarUFPeloCodigo()
+        {
+            var uf = UF.EncontraPeloCodigo(26);
+        }
+
+        [Test]
+        public void DeveEncontrarUFPelaSigla()
+        {
+            var uf = UF.EncontraPelaSigla("PE");
+        }
+
+        [Test]
+        public void TestaAComparacaoEntreUFs()
+        {
+            var pernambuco = UF.PE;
+            var sergipe = UF.SE;
+            var ceara = UF.CE;
+
+            Assert.AreEqual(pernambuco.CompareTo(sergipe), -1);
+            Assert.AreEqual(pernambuco.CompareTo(pernambuco), 0);
+            Assert.AreEqual(pernambuco.CompareTo(ceara), 1);
+        }
+
+        [Test]
+        public void ComparaPeloNomeDoEstado()
+        {
+            var pernambuco = UF.PE;
+            var matoGrosso = UF.MT;
+            var matoGrossoDoSul = UF.MS;
+            IComparer<UF> comparer = new ComparaUFPeloNomeDoEstado();
+
+            Assert.AreEqual(comparer.Compare(pernambuco, matoGrosso), 1);
+            Assert.AreEqual(comparer.Compare(pernambuco, pernambuco), 0);
+            Assert.AreEqual(comparer.Compare(matoGrosso, matoGrossoDoSul), -1);
+        }
+
+        [Test]
+        public void ComparaPelaSigla()
+        {
+            var pernambuco = UF.PE;
+            var matoGrosso = UF.MT;
+            var matoGrossoDoSul = UF.MS;
+            IComparer<UF> comparer = new ComparaUFPelaSigla();
+
+            Assert.AreEqual(comparer.Compare(pernambuco, matoGrosso), 1);
+            Assert.AreEqual(comparer.Compare(pernambuco, pernambuco), 0);
+            Assert.AreEqual(comparer.Compare(matoGrosso, matoGrossoDoSul), 1);
+        }
+    }
+}
