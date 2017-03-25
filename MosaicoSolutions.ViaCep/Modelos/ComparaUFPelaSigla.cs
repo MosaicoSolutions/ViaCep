1b41eb4fc03082df867c408235e149750301060a
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-25 00:54:37 -0300

    Comparador de UF's pela sigla.

---
+++ MosaicoSolutions.ViaCep/Modelos/ComparaUFPelaSigla.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace MosaicoSolutions.ViaCep.Modelos
+{
+    public class ComparaUFPelaSigla : IComparer<UF>
+    {
+        public int Compare(UF x, UF y)
+        {
+            if (ReferenceEquals(x, y))
+                return 0;
+
+            if (ReferenceEquals(null, y))
+                return 1;
+
+            return string.Compare(x?.Sigla, y.Sigla);
+        }
+    }
+}
