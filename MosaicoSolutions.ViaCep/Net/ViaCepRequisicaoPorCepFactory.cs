763a1e7fa997e3dadb5587b09f491d21620b94f2
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-27 10:13:27 -0300

    Adição da classe ViaCepRequisicaoPorCepFactory.

---
+++ MosaicoSolutions.ViaCep/Net/ViaCepRequisicaoPorCepFactory.cs
@@ -0,0 +1,25 @@
+﻿using MosaicoSolutions.ViaCep.Modelos;
+
+namespace MosaicoSolutions.ViaCep.Net
+{
+    /// <summary>
+    /// Uma fábrica para a criação de requisições por Cep.
+    /// </summary>
+    public sealed class ViaCepRequisicaoPorCepFactory : IViaCepRequisicaoPorCepFactory
+    {
+        public IViaCepRequisicaoPor<Cep> NovaRequisicaoJson(Cep cep)
+            => novaRequisicao(cep, ViaCepFormatoRequisicao.Json);
+
+        public IViaCepRequisicaoPor<Cep> NovaRequisicaoXml(Cep cep)
+            => novaRequisicao(cep, ViaCepFormatoRequisicao.Xml);
+
+        public IViaCepRequisicaoPor<Cep> NovaRequisicaoPiped(Cep cep)
+            => novaRequisicao(cep, ViaCepFormatoRequisicao.Piped);
+
+        public IViaCepRequisicaoPor<Cep> NovaRequisicaoQuerty(Cep cep)
+            => novaRequisicao(cep, ViaCepFormatoRequisicao.Querty);
+
+        private IViaCepRequisicaoPor<Cep> novaRequisicao(Cep cep, ViaCepFormatoRequisicao formato)
+            => new ViaCepRequisicaoPorPorCep(cep, formato);
+    }
+}
