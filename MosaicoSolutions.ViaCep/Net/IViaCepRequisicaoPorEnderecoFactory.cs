eeb0de0a2587796ae677e728446e1c82fa6df4e4
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-27 10:09:42 -0300

    Adição da interface IViaCepRequisicaoPorEnderecoFactory.

---
+++ MosaicoSolutions.ViaCep/Net/IViaCepRequisicaoPorEnderecoFactory.cs
@@ -0,0 +1,12 @@
+﻿using MosaicoSolutions.ViaCep.Modelos;
+
+namespace MosaicoSolutions.ViaCep.Net
+{
+    /// <summary>
+    /// Define um Factory para a criação de requisições por Endereço.
+    /// </summary>
+    public interface IViaCepRequisicaoPorEnderecoFactory : IViaCepRequisicaoJsonDe<EnderecoRequisicao>, IViaCepRequisicaoXmlDe<EnderecoRequisicao>
+    {
+
+    }
+}
