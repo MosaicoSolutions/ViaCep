9b1a82c3b8d89a7c4117e1cd97480d90c40212be
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-26 23:11:50 -0300

    Adição da interface IViaCepCliente.

---
+++ MosaicoSolutions.ViaCep/Net/IViaCepCliente.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace MosaicoSolutions.ViaCep.Net
+{
+    /// <summary>
+    /// Cliente responsável por retornar a resposta de uma requisição a partir do Uri do recurso.
+    /// </summary>
+    public interface IViaCepCliente
+    {
+        /// <summary>
+        /// Obtém a resposta, de forma assíncrona, a partir do Uri do recurso.
+        /// </summary>
+        /// <param name="uri">Uri do recurso.</param>
+        /// <returns>A resposta contendo os dados da requisição.</returns>
+        Task<IViaCepResposta> ObterRespostaAsync(IViaCepUri uri);
+
+        /// <summary>
+        /// Obtém a resposta a partir do Uri do recurso.
+        /// </summary>
+        /// <param name="uri">Uri do recurso.</param>
+        /// <returns>A resposta contendo os dados da requisição.</returns>
+        IViaCepResposta ObterResposta(IViaCepUri uri);
+    }
+}
