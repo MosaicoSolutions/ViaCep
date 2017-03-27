64bf27094d06d349f29a11f4b7538b031e435e9e
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-27 10:13:56 -0300

    Adição da classe ViaCepRequisicaoPorEnderecoFactory.

---
+++ MosaicoSolutions.ViaCep/Net/ViaCepRequisicaoPorEnderecoFactory.cs
@@ -0,0 +1,19 @@
+﻿using MosaicoSolutions.ViaCep.Modelos;
+
+namespace MosaicoSolutions.ViaCep.Net
+{
+    /// <summary>
+    /// Uma fábrica para a criação de requisições por Endereço.
+    /// </summary>
+    public class ViaCepRequisicaoPorEnderecoFactory : IViaCepRequisicaoPorEnderecoFactory
+    {
+        public IViaCepRequisicaoPor<EnderecoRequisicao> NovaRequisicaoJson(EnderecoRequisicao enderecoRequisicao)
+            => NovaRequisicao(enderecoRequisicao, ViaCepFormatoRequisicao.Json);
+
+        public IViaCepRequisicaoPor<EnderecoRequisicao> NovaRequisicaoXml(EnderecoRequisicao enderecoRequisicao)
+            => NovaRequisicao(enderecoRequisicao, ViaCepFormatoRequisicao.Xml);
+
+        private IViaCepRequisicaoPor<EnderecoRequisicao> NovaRequisicao(EnderecoRequisicao enderecoRequisicao, ViaCepFormatoRequisicao formato)
+            => new ViaCepRequisicaoPorPorEndereco(enderecoRequisicao, formato);
+    }
+}
