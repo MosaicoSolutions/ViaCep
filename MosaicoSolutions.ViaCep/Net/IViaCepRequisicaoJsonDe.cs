e9d7fca2987d7ce7f553a29e66f5c9466750f786
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-27 10:00:58 -0300

    Adição da interface IViaCepRequisicaoJsonDe<T>.

---
+++ MosaicoSolutions.ViaCep/Net/IViaCepRequisicaoJsonDe.cs
@@ -0,0 +1,18 @@
+﻿using MosaicoSolutions.ViaCep.Modelos;
+
+namespace MosaicoSolutions.ViaCep.Net
+{
+    /// <summary>
+    /// Define uma requisição Json.
+    /// </summary>
+    /// <typeparam name="T">O tipo do objeto contendo os dados da requisição. <see cref="Cep"/> ou <see cref="EnderecoRequisicao"/></typeparam>
+    public interface IViaCepRequisicaoJsonDe<T>
+    {
+        /// <summary>
+        /// Cria uma nova requisição Json.
+        /// </summary>
+        /// <param name="obj">O objeto contendo os dados da requisição.</param>
+        /// <returns>Um objeto que representa o contexto da requisição.</returns>
+        IViaCepRequisicaoPor<T> NovaRequisicaoJson(T obj);
+    }
+}
