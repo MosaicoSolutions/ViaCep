072b19969c48e036e2145727a5b68a6909591288
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-27 10:05:31 -0300

    Adição da interface IViaCepRequisicaoQuertyDe<T>.

---
+++ MosaicoSolutions.ViaCep/Net/IViaCepRequisicaoQuertyDe.cs
@@ -0,0 +1,16 @@
+﻿namespace MosaicoSolutions.ViaCep.Net
+{
+    /// <summary>
+    /// Define uma requisição Querty.
+    /// </summary>
+    /// <typeparam name="T">O tipo do objeto contendo os dados da requisição.</typeparam>
+    public interface IViaCepRequisicaoQuertyDe<T>
+    {
+        /// <summary>
+        /// Cria uma nova requisição Querty.
+        /// </summary>
+        /// <param name="obj">O objeto contendo os dados da requisição.</param>
+        /// <returns>Um objeto que representa o contexto da requisição.</returns>
+        IViaCepRequisicaoPor<T> NovaRequisicaoQuerty(T obj);
+    }
+}
