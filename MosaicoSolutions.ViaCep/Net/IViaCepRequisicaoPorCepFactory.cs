a36c561001b3e62fe1343e904473046169c66083
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-27 10:09:25 -0300

    Adição da interface IViaCepRequisicaoPorCepFactory.

---
+++ MosaicoSolutions.ViaCep/Net/IViaCepRequisicaoPorCepFactory.cs
@@ -0,0 +1,12 @@
+﻿using MosaicoSolutions.ViaCep.Modelos;
+
+namespace MosaicoSolutions.ViaCep.Net
+{
+    /// <summary>
+    /// Define um Factory para a criação de requisições por Cep.
+    /// </summary>
+    public interface IViaCepRequisicaoPorCepFactory : IViaCepRequisicaoJsonDe<Cep>, IViaCepRequisicaoPipedDe<Cep>, IViaCepRequisicaoQuertyDe<Cep>, IViaCepRequisicaoXmlDe<Cep>
+    {
+
+    }
+}
