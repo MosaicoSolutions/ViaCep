3c5288c4fee7b8121d24b6d77301b2d02149e11f
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-27 10:38:29 -0300

    Adição da interface IViaCepResposta.

---
+++ MosaicoSolutions.ViaCep/Net/IViaCepResposta.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+namespace MosaicoSolutions.ViaCep.Net
+{
+    /// <summary>
+    /// Define a resposta de uma requisição.
+    /// </summary>
+    public interface IViaCepResposta
+    {
+        /// <summary>
+        /// Obtém o código de status da requisição.
+        /// </summary>
+        HttpStatusCode CodigoDeStatus { get; }
+
+        /// <summary>
+        /// Avalia se é código de sucesso. <see cref="HttpStatusCode.OK"/> => 200.
+        /// </summary>
+        bool EhCodigoDeSucesso { get; }
+
+        /// <summary>
+        /// Obtém o conteúdo da requisição.
+        /// </summary>
+        /// <returns></returns>
+        IViaCepConteudo ObterConteudo();
+    }
+}
