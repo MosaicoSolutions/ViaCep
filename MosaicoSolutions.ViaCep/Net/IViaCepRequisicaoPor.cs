616f074b806246113a19cafff527dcbcb01bd3f9
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-27 09:42:34 -0300

    Adição da interface IViaCepRequisicaoPor<out T>.

---
+++ MosaicoSolutions.ViaCep/Net/IViaCepRequisicaoPor.cs
@@ -0,0 +1,21 @@
+﻿using MosaicoSolutions.ViaCep.Modelos;
+
+namespace MosaicoSolutions.ViaCep.Net
+{
+    /// <summary>
+    /// Representa uma requisição ViaCep contendo o objeto com os dados da requisição e um formato.
+    /// </summary>
+    /// <typeparam name="T">O tipo do objeto que contém os dados da requisição, <see cref="Cep"/> ou <see cref="EnderecoRequisicao"/>.</typeparam>
+    public interface IViaCepRequisicaoPor<out T> : IViaCepUri
+    {
+        /// <summary>
+        /// Objeto contendo os dados da requisição.
+        /// </summary>
+        T ObjetoDaRequisicao { get; }
+
+        /// <summary>
+        /// O formato desta requisição.
+        /// </summary>
+        ViaCepFormatoRequisicao FormatoRequisicao { get; }
+    }
+}
