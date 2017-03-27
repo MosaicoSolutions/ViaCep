a3ccb9ce51fc0fdd505c9ab10e67f063d79a91fd
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-27 10:04:30 -0300

    Adição da interface IViaCepRequisicaoPipedDe<T>.

---
+++ MosaicoSolutions.ViaCep/Net/IViaCepRequisicaoPipedDe.cs
@@ -0,0 +1,16 @@
+﻿namespace MosaicoSolutions.ViaCep.Net
+{
+    /// <summary>
+    /// Define uma requisição Piped.
+    /// </summary>
+    /// <typeparam name="T">O tipo do objeto contendo os dados da requisição.</typeparam>
+    public interface IViaCepRequisicaoPipedDe<T>
+    {
+        /// <summary>
+        /// Cria uma nova requisição Piped.
+        /// </summary>
+        /// <param name="obj">O objeto contendo os dados da requisição.</param>
+        /// <returns>Um objeto que representa o contexto da requisição.</returns>
+        IViaCepRequisicaoPor<T> NovaRequisicaoPiped(T obj);
+    }
+}
