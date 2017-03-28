085779095f841c080b31ab38c65a7f53963c0dbb
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-28 12:34:42 -0300

    Adição do método ToString().

--- MosaicoSolutions.ViaCep/Modelos/Endereco.cs
+++ MosaicoSolutions.ViaCep/Modelos/Endereco.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace MosaicoSolutions.ViaCep.Modelos
 {
@@ -14,5 +15,20 @@ namespace MosaicoSolutions.ViaCep.Modelos
         public string Unidade { get; set; }
         public string IBGE { get; set; }
         public string GIA { get; set; }
+
+        public override string ToString()
+        {
+            const string separador = " : ";
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(nameof(Cep)).Append(separador).Append(Cep).Append(Environment.NewLine);
+            stringBuilder.Append(nameof(Logradouro)).Append(separador).Append(Logradouro).Append(Environment.NewLine);
+            stringBuilder.Append(nameof(Complemento)).Append(separador).Append(Complemento).Append(Environment.NewLine);
+            stringBuilder.Append(nameof(Localidade)).Append(separador).Append(Localidade).Append(Environment.NewLine);
+            stringBuilder.Append(nameof(UF)).Append(separador).Append(UF).Append(Environment.NewLine);
+            stringBuilder.Append(nameof(Unidade)).Append(separador).Append(Unidade).Append(Environment.NewLine);
+            stringBuilder.Append(nameof(IBGE)).Append(separador).Append(IBGE).Append(Environment.NewLine);
+            stringBuilder.Append(nameof(GIA)).Append(separador).Append(GIA).Append(Environment.NewLine);
+            return stringBuilder.ToString();
+        }
     }
 }
