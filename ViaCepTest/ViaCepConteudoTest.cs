fda7f21b499fc60cd713871745efb4be0d200a28
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-03-30 11:36:49 -0300

    Teste Unitário para a interface IViaCepConteudo.

---
+++ ViaCepTest/ViaCepConteudoTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Xml.Linq;
+using MosaicoSolutions.ViaCep.Net;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace ViaCepTest
+{
+    [TestFixture]
+    public class ViaCepConteudoTest
+    {
+        private MockRepository _mockRepository;
+        private IViaCepConteudo _conteudo;
+
+        private const string XmlContent = @"<xmlcep>
+                                            <cep>01001-000</cep>
+                                            <logradouro>Praça da Sé</logradouro>
+                                            <complemento>lado ímpar</complemento>
+                                            <bairro>Sé</bairro>
+                                            <localidade>São Paulo</localidade>
+                                            <uf>SP</uf>
+                                            <unidade/>
+                                            <ibge>3550308</ibge>
+                                            <gia>1004</gia>
+                                            </xmlcep>";
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new MockRepository();
+            _conteudo = _mockRepository.StrictMock<IViaCepConteudo>();
+        }
+
+        [Test]
+        public void DeveSerLidoComoXml()
+        {
+            _conteudo = _mockRepository.StrictMock<IViaCepConteudo>();
+            _conteudo.Expect(m => m.PodeSerLidoComoXml).Return(true).Repeat.Once();
+            _conteudo.Expect(m => m.LerComoString()).Return(XmlContent).Repeat.Once();
+            _conteudo.Expect(m => m.LerComoXml()).Return(XDocument.Parse(XmlContent)).Repeat.Once();
+            _conteudo.Expect(m => m.PossuiErro).Return(true).Repeat.Once();
+
+            _conteudo.Replay();
+
+            if (!_conteudo.PodeSerLidoComoXml)
+                throw new AssertionException("O conteudo não pode ser lido como xml.");
+
+            Assert.AreEqual(XmlContent, _conteudo.LerComoString());
+
+            Assert.DoesNotThrow(() =>
+            {
+                _conteudo.LerComoXml();
+            });
+        }
+    }
+}
