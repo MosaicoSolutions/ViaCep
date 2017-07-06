e207cac6ca4476a1451e8d3437b2af81e58c31dc
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-07-05 22:28:08 -0300

    Adição da classe ComparaUFPeloCodigo.

---
+++ MosaicoSolutions.ViaCep/Modelos/ComparaUFPeloCodigo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MosaicoSolutions.ViaCep.Modelos
+{
+    public sealed class ComparaUFPeloCodigo : IComparer<UF>
+    {
+        public int Compare(UF x, UF y)
+            => ReferenceEquals(x, y) ? 
+                0 : ReferenceEquals(null, x) ? 
+                    -1 : ReferenceEquals(null, y) ? 
+                        1 : x.Codigo.CompareTo(y.Codigo);
+    }
+}
