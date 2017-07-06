ca60a9ad1a08b41c8ae08cf6664e812868a3264e
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-07-05 23:51:53 -0300

    Mudança de package.

--- ViaCepTest/MosaicoSolutions/ViaCep/Teste/Net/ViaCepConteudoTest.cs
+++ ViaCepTest/MosaicoSolutions/ViaCep/Teste/Net/ViaCepConteudoTest.cs
@@ -3,7 +3,7 @@ using MosaicoSolutions.ViaCep.Net;
 using NUnit.Framework;
 using Rhino.Mocks;
 
-namespace ViaCepTest
+namespace ViaCepTest.MosaicoSolutions.ViaCep.Teste.Net
 {
     [TestFixture]
     public class ViaCepConteudoTest
@@ -46,10 +46,7 @@ namespace ViaCepTest
 
             Assert.AreEqual(XmlContent, _conteudo.LerComoString());
 
-            Assert.DoesNotThrow(() =>
-            {
-                _conteudo.LerComoXml();
-            });
+            Assert.DoesNotThrow(() => _conteudo.LerComoXml());
         }
     }
 }
