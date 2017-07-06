4fafd080ff0bcae72c0945241eb01f46f4ae9131
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-07-05 22:31:36 -0300

    Adição do teste unitário da classe Regiao.

---
+++ ViaCepTest/MosaicoSolutions/ViaCep/Teste/Modelos/RegiaoTest.cs
@@ -0,0 +1,19 @@
+﻿using MosaicoSolutions.ViaCep.Modelos;
+using NUnit.Framework;
+
+namespace ViaCepTest.MosaicoSolutions.ViaCep.Teste.Modelos
+{
+    [TestFixture]
+    public class RegiaoTest
+    {
+        [Test]
+        public void DeveConterOEstado()
+        {
+            Assert.True(Regiao.CentroOeste.ContemOEstado(UF.DF));
+            Assert.True(Regiao.Nordeste.ContemOEstado(UF.PE));
+            Assert.True(Regiao.Norte.ContemOEstado(UF.AM));
+            Assert.True(Regiao.Sudeste.ContemOEstado(UF.RJ));
+            Assert.True(Regiao.Sul.ContemOEstado(UF.SC));
+        }
+    }
+}
