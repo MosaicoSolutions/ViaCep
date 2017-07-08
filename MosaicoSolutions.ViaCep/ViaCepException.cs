f44e845de686f6f2940610c8283c4b6058d6c9d3
brunoSpeedrun <bruno.xavier.moura@gmail.com>
2017-07-08 00:22:29 -0300

    Ajustes na classe.

--- MosaicoSolutions.ViaCep/ViaCepException.cs
+++ MosaicoSolutions.ViaCep/ViaCepException.cs
@@ -6,7 +6,7 @@ using System.Runtime.Serialization;
 namespace MosaicoSolutions.ViaCep
 {
     /// <summary>
-    /// A exceção que é lançada quando o ocorre algum erro em uma requisição ao serviço ViaCep.
+    /// A exceção que é lançada quando o ocorre algum erro em uma requisição ao serviço ViaCepService.
     /// </summary>
     [Serializable]
     public class ViaCepException : Exception
@@ -16,19 +16,13 @@ namespace MosaicoSolutions.ViaCep
         /// <summary>
         /// Inicializa uma nova instância de <see cref="ViaCepException"/>.
         /// </summary>
-        public ViaCepException()
-        {
-            _message = Util.MensagensDeErro.ErroDeRequisicao;
-        }
+        public ViaCepException() => _message = Util.MensagensDeErro.ErroDeRequisicao;
 
         /// <summary>
         /// Inicializa uma nova instância de <see cref="ViaCepException"/> com a mensagem de erro especificada.
         /// </summary>
         /// <param name="message">A mensagem de erro que explica o motivo da exceção.</param>
-        public ViaCepException(string message)
-        {
-            _message = message;
-        }
+        public ViaCepException(string message) => _message = message;
 
         /// <summary>
         /// Inicializa uma nova instância da class <code>ViaCepException</code> com a mensagem de erro especificada.
@@ -39,18 +33,14 @@ namespace MosaicoSolutions.ViaCep
         /// Se o parâmetro <see cref="Exception.InnerException"/> não é uma referência nula,
         /// a exceção atual é gerada em um bloco catch que manipula a exceção interna.
         /// </param>
-        public ViaCepException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
+        public ViaCepException(string message, Exception innerException) : base(message, innerException) { }
 
         /// <summary>
         /// Inicializa uma nova instância de <see cref="ViaCepException"/> com dados serializados.
         /// </summary>
         /// <param name="info">O objeto que contém os dados do objeto serializado.</param>
         /// <param name="context">As informações contextuais sobre a origem ou o destino.</param>
-        protected ViaCepException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
+        protected ViaCepException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 
         internal ViaCepException(HttpStatusCode codigoDeStatus)
         {
