21252b35a4965395c32c3f96afa46afb588b4c55
Bruno X. Moura <bruno.xavier.moura@gmail.com>
2018-02-28 22:29:49 -0300

    Ajustes no XmlDocs e na ordem das Propriedades.

--- MosaicoSolutions.ViaCep/Net/ViaCepFormatoRequisicao.cs
+++ MosaicoSolutions.ViaCep/Net/ViaCepFormatoRequisicao.cs
@@ -8,11 +8,6 @@ namespace MosaicoSolutions.ViaCep.Net
     [Serializable]
     public sealed class ViaCepFormatoRequisicao : IEquatable<ViaCepFormatoRequisicao>
     {
-        /// <summary>
-        /// O valor do formato da requisição em string. Exemplo: ViaCepFormatoRequisicao.Json.Valor => "json".
-        /// </summary>
-        public string Valor { get; }
-
         public static readonly ViaCepFormatoRequisicao Json = new ViaCepFormatoRequisicao("json");
 
         public static readonly ViaCepFormatoRequisicao Xml = new ViaCepFormatoRequisicao("xml");
@@ -20,18 +15,26 @@ namespace MosaicoSolutions.ViaCep.Net
         public static readonly ViaCepFormatoRequisicao Piped = new ViaCepFormatoRequisicao("piped");
 
         public static readonly ViaCepFormatoRequisicao Querty = new ViaCepFormatoRequisicao("querty");
+        
+        /// <summary>
+        /// O valor do formato da requisição em string. Exemplo: ViaCepFormatoRequisicao.Json.Valor => "json".
+        /// </summary>
+        public string Valor { get; }
 
         private ViaCepFormatoRequisicao(string value) => Valor = value;
 
+        /// <inheritdoc />
         public override bool Equals(object obj)
-            => obj is ViaCepFormatoRequisicao && Equals((ViaCepFormatoRequisicao)obj);
+            => obj is ViaCepFormatoRequisicao && Equals((ViaCepFormatoRequisicao) obj);
 
+        /// <inheritdoc />
         public bool Equals(ViaCepFormatoRequisicao other)
             => other != null && Valor == other.Valor;
 
+        /// <inheritdoc />
         public override int GetHashCode()
             => Valor?.GetHashCode() ?? 0;
-
+        
         public override string ToString() => Valor;
     }
 }
