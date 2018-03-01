ec192323dfb2872a37bfcac3e513f736f139fea4
Bruno X. Moura <bruno.xavier.moura@gmail.com>
2018-02-28 22:42:01 -0300

    Retirada do atributo 'CepInvalido' não utilizado.

--- MosaicoSolutions.ViaCep/Util/MensagensDeErro.cs
+++ MosaicoSolutions.ViaCep/Util/MensagensDeErro.cs
@@ -4,17 +4,14 @@ namespace MosaicoSolutions.ViaCep.Util
 {
     internal static class MensagensDeErro
     {
-        internal static readonly string CepInvalido =
-            "O Cep não estava em um formato válido. Um dos seguintes formatos eram esperados: 00000000 ou 00000-000";
+        internal const string ErroDeRequisicao = "Ocorreu um erro ao tentar obter o(s) endereço(s).";
 
-        internal static readonly string ErroDeRequisicao = "Ocorreu um erro ao tentar obter o(s) endereço(s).";
-
+        internal const string CepInexistente = "O Cep especificado não existe.";
+        
         internal static readonly string BadRequest = ErroDeRequisicao + Environment.NewLine +
                                                      "A requisicao está mal formada.";
 
         internal static readonly string AutenticacaoProxyRequerida = ErroDeRequisicao + Environment.NewLine +
                                                                      "Autenticação de proxy é requerida";
-
-        internal static readonly string CepInexistente = "O Cep especificado não existe.";
     }
 }
